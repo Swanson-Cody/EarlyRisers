2f56c4df3f41e7642e9900f13bc1684039f80421
Heather <Heather@DESKTOP-1NKH1N6>
2018-10-31 07:31:09 -0700

    Making View Layer

---
+++ EarlyRisers/src/View/StartExistingGameView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ *
+ * @author Heather
+ */
+public class StartExistingGameView {
+    
+    
+    restartNewGame(); void {
+        startExistingGameView = Create a new StartExistingGameView
+        startExistingGameView = displayStartExistingGameView();}
+}
