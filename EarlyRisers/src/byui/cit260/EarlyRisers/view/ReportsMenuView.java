c100bec6a4009ec6e4fcffb9b8be59ef57659b99
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-15 18:44:12 -0700

    Changed Reports Menu choices

--- EarlyRisers/src/byui/cit260/EarlyRisers/view/ReportsMenuView.java
+++ EarlyRisers/src/byui/cit260/EarlyRisers/view/ReportsMenuView.java
@@ -11,91 +11,84 @@ import java.util.Scanner;
  *
  * @author Family
  */
-public class ReportsMenuView {
-    boolean exit;
-    
-    public void display() {
-	        printReportsMenu();
-	        while (!exit){
-	            printReportsMenu();
-	            String choice = getInput();
-	            doAction(choice);
-	        }
-	}
-    
-    private void printReportsMenu() {
-        boolean done = false;
-        
-        do{
-        System.out.println("+-------------------------------------------+");
-        System.out.println("|                                           |");
-        System.out.println("|                Reports Menu               |");
-        System.out.println("|                                           |");
-        System.out.println("|                                           |");
-        System.out.println("+-------------------------------------------+");
-        
-        System.out.println("A: View the animals in the storehouse");
-        System.out.println("T: View the tools in the storehouse");
-        System.out.println("P: View the provisions in the storehouse");
-        System.out.println("X: View the authors of this game");
-        System.out.println("E: Exit Reports Menu screen");
-        String input = getInput();
-        done = doAction(input);
-        
-        } while(!done);
+public class ReportsMenuView extends View {
 
-    } 
-    
-    
+    boolean exit;
+
+    public void display() {
+    }
+
+    //Get number input from user 
+    @Override
+    public String[] getInputs() {
+        String[] inputs = new String[10];
+        String selection = this.getInput(
+                "\n+-------------------------------------------+"
+                + "\n|                                           |"
+                + "\n|                Reports Menu               |"
+                + "\n|                                           |"
+                + "\n|                                           |"
+                + "\n+-------------------------------------------+"
+                + "\n1: View the animals in the storehouse"
+                + "\n2: View the tools in the storehouse"
+                + "\n3: View the provisions in the storehouse"
+                + "\n4: View the authors of this game"
+                + "\n5: Exit Reports Menu screen");
+        inputs[0] = selection;
+        return inputs;
+
+    }
+
     //Get input from user 
-    private String getInput(){
+    private String getInput() {
         Scanner keyboard = new Scanner(System.in);
         String choice = keyboard.nextLine();
         return choice;
     }
-    private void viewAnimals(){
+
+    private void viewAnimals() {
         System.out.println("You're Viewing Animals!");
     }
-    private void viewTools(){
+
+    private void viewTools() {
         System.out.println("You're Viewing Tools!");
     }
-    private void viewProvisions(){
+
+    private void viewProvisions() {
         System.out.println("You're Viewing Provisions!");
     }
-    private void viewAuthors(){
+
+    private void viewAuthors() {
         System.out.println("You're Viewing Authors!");
     }
-    private void goToGameMenu(){
+
+    private void goToGameMenu() {
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.display();
     }
-    
-    private boolean doAction(String choice){
-       choice = choice.toUpperCase();
-    
-       if("E".equals(choice)){
+    @Override
+    public boolean doAction(String[] input){
+        String choice = input[0].toUpperCase();
+        choice = choice.toUpperCase();
+
+        if ("5".equals(choice)) {
             System.out.println("You have exited the Reports Menu. "
                     + "Have fun with the game!");
             goToGameMenu();
             return true;
-       }
-        else if("A".equals(choice)){
+        } else if ("1".equals(choice)) {
             viewAnimals();
-        }
-        else if("T".equals(choice)){ 
+        } else if ("2".equals(choice)) {
             viewTools();
-        }
-        else if("P".equals(choice)){
+        } else if ("3".equals(choice)) {
             viewProvisions();
-        }
-        else if("X".equals(choice)){
+        } else if ("4".equals(choice)) {
             viewAuthors();
-        }
-        else{
+        } else {
             System.out.println("\nInvalid selection.");
         }
-       
-    return false;
+
+        return false;
     }
 
 }
