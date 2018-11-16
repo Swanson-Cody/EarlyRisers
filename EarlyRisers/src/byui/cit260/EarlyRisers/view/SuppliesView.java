ed461029960909298f15e506156aa6fec1e667e5
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-15 18:50:31 -0700

    Changed Supplies Menu choices

--- EarlyRisers/src/byui/cit260/EarlyRisers/view/SuppliesView.java
+++ EarlyRisers/src/byui/cit260/EarlyRisers/view/SuppliesView.java
@@ -4,13 +4,15 @@
  * and open the template in the editor.
  */
 package byui.cit260.EarlyRisers.view;
+
 import java.util.Scanner;
+
 /**
  *
  * @author rache
  */
-public class SuppliesView {
-    
+public class SuppliesView extends View {
+
     boolean exit;
 //    public void displaySupplies() {
 //
@@ -20,83 +22,76 @@ public class SuppliesView {
 //
 //    }
 //    
-                
-	    public void display() {
-	        printSuppliesHeader();
-	        while (!exit){
-	            printSuppliesHeader();
-	            String choice = getInput();
-	            doAction(choice);
-	        }
-	}
 
-    //Welcome Screen
-    private void printSuppliesHeader() {
-        boolean done = false;
+    public void display() {}
+        //Get number input from user 
+        @Override
+        public String[] getInputs
         
-        do{
-        System.out.println("+-------------------------------------------+");
-        System.out.println("|                                           |");
-        System.out.println("|                 Supplies                  |");
-        System.out.println("|                                           |");
-        System.out.println("|                                           |");
-        System.out.println("+-------------------------------------------+");
-        
-        System.out.println("M: How much Money You have");
-        System.out.println("P: What is the current population");
-        System.out.println("W: How much wheat do you have");
-        System.out.println("E: Exit supplies screen");
-        String input = getInput();
-        done = doAction(input);
-        
-        } while(!done);
-
-    } 
-    
-      //Get input from user 
-    private String getInput(){
+            (){
+        String[] inputs = new String[10];
+            String selection = this.getInput(
+                    "+-------------------------------------------+"
+                    + "|                                           |"
+                    + "|                 Supplies                  |"
+                    + "|                                           |"
+                    + "|                                           |"
+                    + "+-------------------------------------------+"
+                    + "1: How much Money You have"
+                    + "2: What is the current population"
+                    + "3: How much wheat do you have"
+                    + "4: Exit supplies screen");
+            inputs[0] = selection;
+            return inputs;
+        }
+        //Get input from user 
+    private String getInput() {
         Scanner keyboard = new Scanner(System.in);
         String choice = keyboard.nextLine();
         return choice;
     }
-        private void calculateMoney(){
+
+    private void calculateMoney() {
         System.out.println("You currently have this much money.");
     }
-            private void calculatePopulation(){
+
+    private void calculatePopulation() {
         System.out.println("The current population is: ");
     }
-            
-    private void calculateWheat(){
+
+    private void calculateWheat() {
         System.out.println("You currently have this much wheat in storage.");
 
     }
-    
-    private void goToMainMenu(){
+
+    private void goToMainMenu() {
         MainMenuView mainMenu = new MainMenuView();
         mainMenu.display();
     }
-    
-    private boolean doAction(String choice){
-       choice = choice.toUpperCase();
-    switch(choice){
-        case "E":
-            System.out.println("You have exited the supplies screen."
-                    + "Have fun with the game!");
-            goToMainMenu();
-            return true;
-        case "M":
-            calculateMoney();
-            break;
-        case "P": 
-            calculatePopulation();
-        break;
-        case "W":
-             calculateWheat();
-        break;
-        default:
-            System.out.println("\nInvalid selection.");
-    }
-    return false;
+
+    @Override
+    public boolean doAction(String[] input) {
+        String choice = input[0].toUpperCase();
+        choice = choice.toUpperCase();
+        switch (choice) {
+            case "4":
+                System.out.println("You have exited the supplies screen."
+                        + "Have fun with the game!");
+                goToMainMenu();
+                return true;
+            case "1":
+                calculateMoney();
+                break;
+            case "2":
+                calculatePopulation();
+                break;
+            case "3":
+                calculateWheat();
+                break;
+            default:
+                System.out.println("\nInvalid selection.");
+        }
+        return false;
     }
 
 }
