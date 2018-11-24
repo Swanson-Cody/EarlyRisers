739f67d3c76fc4767acd38fbd3123e1bc96a3a10
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-24 14:40:06 -0700

    Map Control beginning not finished yet

---
+++ EarlyRisers/src/byui/cit260/EarlyRisers/control/MapControl.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.EarlyRisers.control;
+
+import byui.cit260.EarlyRisers.model.Map /**
+         *
+         * @author Heather
+         */
+
+public class MapControl {
+
+    private static final int MAX_ROW = 5;
+    private static final int MAX_COL = 5;
+   public static Map createMap() {  
+      if (returnValue == null) {
+            return -1;
+        }
+       if (MAX_ROW <= 0){
+         }
+    if (MAX_COL <= 0){
+                }
+     Map theMap = new Map();public static Location[][] createLocations(int rows, int columns){}
+    private static int assignItemsToLocations(Location[][] locations,
+            InventoryItem[] itemsInGame);
+
+   
+        Map theMap = new Map();
+        Location loc = new Location();
+        int returnValue = GameControl.saveGame(currentGame);
+        
+
+        if (assignItemsToLocations == null) {
+        }
+    }
+public static Location[][] createLocations(int rows, int columns){}
+    
+public static createLocations(){}
+    if (createLocations () == null){
+        return -1;
+}call setters to assign row, column
+call setters to assign description
+call setters to assign display symbol
+call setter method to assign values
+return locations
+    {
+private static int assignItemsToLocations (Location [][] locations, 
+InventoryItem[] itemsInGame) {}
+    if (assignItemsToLocations () == null){ 
+    return -1;
+}
+    }
+
+
+}
+}
+
+////public static Map createMap( Game game,
+// int noOfRows,
+// int noOfColumns) {
+//// check for invalid inputs
+//if game is null noOfRows < 0 OR numOfColumns < 0
+// return null
+//endif
+//
+//// create the map object and assign values to it
+//map = a new Map object
+//call setter to assign value to description in map
+//call setter to assign value to noOfRows in the map
+//call setter to assign value to noOfColumns in map
+//call setter to save the map in game object
+//// create a two-dimensional array of locations and assign array to the map
+//locations = createLocations(noOfRows, noOfColumns)
+//if (locations is null) then
+// return null
+//endif
+//call setter to save a locations array in the map object
+//// assign actors and items to locations
+//success = assignActorsToLocations(locations)
+//if (success < 0) then
+// return null
+//endif
+//success = assignIemsToLocations(locations)
+//if (success < 0) then
+// return null
+//endif
+//// Assign all other types objects to locations (e.g., questions, spells)
+//…
+//return map
+//}
+
+
+//int location [][] = new location [5][5];
+    location[1][0]
+    location[2][0]
+    location[3][0]
