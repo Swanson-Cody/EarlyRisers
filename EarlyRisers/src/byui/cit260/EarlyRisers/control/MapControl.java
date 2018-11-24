8e3c2bed5ca92c3c041812544b068acc064004b3
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-24 15:43:18 -0700

    Map Control beginning not finished yet

--- EarlyRisers/src/byui/cit260/EarlyRisers/control/MapControl.java
+++ EarlyRisers/src/byui/cit260/EarlyRisers/control/MapControl.java
@@ -5,68 +5,50 @@
  */
 package byui.cit260.EarlyRisers.control;
 
-import byui.cit260.EarlyRisers.model.Map /**
-         *
-         * @author Heather
-         */
+import byui.cit260.EarlyRisers.model.Map;
+
+/**
+ *
+ *
+ */
 
 public class MapControl {
+    //public static Map createMap( Game game,
 
-    private static final int MAX_ROW = 5;
-    private static final int MAX_COL = 5;
-   public static Map createMap() {  
-      if (returnValue == null) {
-            return -1;
-        }
-       if (MAX_ROW <= 0){
-         }
-    if (MAX_COL <= 0){
-                }
-     Map theMap = new Map();public static Location[][] createLocations(int rows, int columns){}
-    private static int assignItemsToLocations(Location[][] locations,
-            InventoryItem[] itemsInGame);
+    public static Map createMap() {
+        // int noOfRows,
+        // int noOfColumns) 
+    int MAX_ROW = 5;
+    int MAX_COL = 5;
 
-   
-        Map theMap = new Map();
-        Location loc = new Location();
-        int returnValue = GameControl.saveGame(currentGame);
-        
-
-        if (assignItemsToLocations == null) {
-        }
-    }
-public static Location[][] createLocations(int rows, int columns){}
-    
-public static createLocations(){}
-    if (createLocations () == null){
-        return -1;
-}call setters to assign row, column
-call setters to assign description
-call setters to assign display symbol
-call setter method to assign values
-return locations
-    {
-private static int assignItemsToLocations (Location [][] locations, 
-InventoryItem[] itemsInGame) {}
-    if (assignItemsToLocations () == null){ 
-    return -1;
-}
-    }
-
-
-}
-}
-
-////public static Map createMap( Game game,
-// int noOfRows,
-// int noOfColumns) {
-//// check for invalid inputs
+    //public static Map createMap( Game game,
+// {
+// check for invalid inputs
 //if game is null noOfRows < 0 OR numOfColumns < 0
 // return null
+
+    if (MAX_ROW <= 0){
+        return == null
+         }
+    else if (MAX_COL<= 0){
+        return == null
+                }
+    else if(game == null){
+        return -1}
+    else {return null}
+    
+    
+
 //endif
 //
 //// create the map object and assign values to it
-//map = a new Map object
+//map = a new Map object 
+Map theMap = new Map();
+Location loc = new Location();
+
+
+public static Location[][] createLocations(int rows, int columns) {
+    }
 //call setter to assign value to description in map
 //call setter to assign value to noOfRows in the map
 //call setter to assign value to noOfColumns in map
@@ -82,6 +64,8 @@ InventoryItem[] itemsInGame) {}
 //if (success < 0) then
 // return null
 //endif
+ private static int assignItemsToLocations(Location[][] locations,
+            InventoryItem[] itemsInGame);
 //success = assignIemsToLocations(locations)
 //if (success < 0) then
 // return null
@@ -90,6 +74,26 @@ InventoryItem[] itemsInGame) {}
 //…
 //return map
 //}
+   
+     
+
+   
+
+  
+    int returnValue = GameControl.saveGame(currentGame);
+
+    if (assignItemsToLocations
+
+    
+    
+
+== null) {
+        }
+    }
+
+
+}
+}
 
 
 //int location [][] = new location [5][5];
