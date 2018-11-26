aa00b4a01b2e88547699e080fac0204c2c78c830
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-26 16:03:30 -0700

    Map Control beginning not finished yet

--- EarlyRisers/src/byui/cit260/EarlyRisers/control/MapControl.java
+++ EarlyRisers/src/byui/cit260/EarlyRisers/control/MapControl.java
@@ -37,17 +37,19 @@ public class MapControl {
             System.out.println("Invalid Location.");
 // create the map object and assign values to it
 //map = a new Map object       }      
-            Map theMap = new Map(int _col, int _row, int );
-            this.currentColumn = currentColumn;
-            this.currentRow = currentRow;
-            this.currentLocation = currentLocation;
-            this.visited = visited;
-            this.locations = locations;
-            this.rowCount = rowCount;
-            this.colCount = colCount;
-//// create the map object and assign values to it
+            Map theMap = new Map(MAX_ROW, MAX_COL);
+// call setter to assign value to description in map
+//call setter to assign value to noOfRows in the map
+//call setter to assign value to noOfColumns in map
+//call setter to save the map in game object   
+// create the map object and assign values to it
 //locations = createLocations(noOfRows, noOfColumns)
+//if (locations is null) then
+// return null
+//endif
+//public static Location[][] createLocations(int rows, int columns)
 //Create the fields
+
             for (int i = 0; i < MAX_ROW; i++) {
                 Location loc1 = new Location();
                 loc1.setDescription("Wheat Fields");
@@ -85,43 +87,30 @@ public class MapControl {
 
             }
        
-    
-    
-
-    
-
+   return theMap;}
+   
     public void displayMap() {
-        Map theMap = theGame.getMap();
+        Game _game = theGame.getMap();
+        
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 8; j++) {
                 System.out.print(theMap.getLocation(i, j).getSymbol() + " ");
             }
             System.out.println();
-        }
+                  }
+//call setter to save a locations array in the map object
 
+//success = assignIemsToLocations(locations)
+//private static int assignItemsToLocations(
+//Location[][] locations,
+//InventoryItem[] itemsInGame)
+//if (success < 0) then
+// return null
+//endif
     }
 }
 
-call setter to assign value to description in map
-call setter to assign value to noOfRows in the map
-call setter to assign value to noOfColumns in map
-call setter to save the map in game object
-// create a two-dimensional array of locations and assign array to the map
-locations = createLocations(noOfRows, noOfColumns)
-if (locations is null) then
- return null
-endif
-call setter to save a locations array in the map object
-// assign actors and items to locations
-success = assignActorsToLocations(locations)
-if (success < 0) then
- return null
-endif
-success = assignIemsToLocations(locations)
-if (success < 0) then
- return null
-endif
 // Assign all other types objects to locations (e.g., questions, spells)
 …
 return map
