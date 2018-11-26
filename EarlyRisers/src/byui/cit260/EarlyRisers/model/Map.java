13c8b1419a14049ad162fcc69e8e70396c58edb8
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-26 13:41:09 -0700

    Map Control beginning not finished yet

--- EarlyRisers/src/byui/cit260/EarlyRisers/model/Map.java
+++ EarlyRisers/src/byui/cit260/EarlyRisers/model/Map.java
@@ -20,66 +20,119 @@ public class Map implements Serializable {
     private int colCount;
     private int currentRow = 0;
     private int currentColumn = 0;
-    private Boolean visited;
-
-    public Map(int MAX_ROW, int MAX_COL) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public String getLocations() {
-        return locations;
-    }
-
-    public void setLocations(String locations) {
-        this.locations = locations;
-    }
-
-    public Location getCurrentLocation() {
-        return currentLocation;
-    }
 
     public void setCurrentLocation(Location currentLocation) {
         this.currentLocation = currentLocation;
-    }
-
-    public int getRowCount() {
-        return rowCount;
-    }
-
-    public void setRowCount(int rowCount) {
-        this.rowCount = rowCount;
-    }
-
-    public int getColCount() {
-        return colCount;
-    }
-
-    public void setColCount(int colCount) {
-        this.colCount = colCount;
-    }
-
-    public int getCurrentRow() {
-        return currentRow;
     }
 
     public void setCurrentRow(int currentRow) {
         this.currentRow = currentRow;
     }
 
-    public int getCurrentColumn() {
-        return currentColumn;
-    }
-
     public void setCurrentColumn(int currentColumn) {
         this.currentColumn = currentColumn;
     }
 
-    public Boolean getVisited() {
-        return visited;
-    }
-
     public void setVisited(Boolean visited) {
         this.visited = visited;
+    }
+
+    private Boolean visited;
+
+    public Map(int MAX_ROW, int MAX_COL) {
+        MAX_ROW = 5;
+        MAX_COL = 5;
+
+    }
+
+    /**
+     * Get the value of locations
+     *
+     * @return the value of locations
+     */
+    public String getLocations() {
+        return locations;
+    }
+
+    /**
+     * Set the value of locations
+     *
+     * @param locations new value of locations
+     */
+    public void setLocations(String locations) {
+        this.locations = locations;
+    }
+
+      public int getRowCount() {
+        return rowCount;
+    }
+
+    public int getColCount() {
+        return colCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public void setColCount(int colCount) {
+        this.colCount = colCount;
+    }
+ public void setLocation(int i, int i0, Location loc) {
+        
+    }
+
+    public Object getLocation(int i, int j) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.locations);
+        hash = 79 * hash + Objects.hashCode(this.currentLocation);
+        hash = 79 * hash + this.rowCount;
+        hash = 79 * hash + this.colCount;
+        hash = 79 * hash + this.currentRow;
+        hash = 79 * hash + this.currentColumn;
+        hash = 79 * hash + Objects.hashCode(this.visited);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.rowCount != other.rowCount) {
+            return false;
+        }
+        if (this.colCount != other.colCount) {
+            return false;
+        }
+        if (this.currentRow != other.currentRow) {
+            return false;
+        }
+        if (this.currentColumn != other.currentColumn) {
+            return false;
+        }
+        if (!Objects.equals(this.locations, other.locations)) {
+            return false;
+        }
+        if (!Objects.equals(this.currentLocation, other.currentLocation)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
