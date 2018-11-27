afc0b7e5fc3bbe824426813080e21d71a3fa9a26
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-26 17:27:50 -0700

    Map Control beginning not finished yet

---
+++ EarlyRisers/src/byui/cit260/EarlyRisers/view/MapView.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.EarlyRisers.view;
+import byui.cit260.EarlyRisers.main.Game;
+import byui.cit260.EarlyRisers.model.Map;
+import byui.cit260.EarlyRisers.control.MapControl;
+import byui.cit260.EarlyRisers.model.Location;
+
+/**
+ *
+ * 
+ */
+public class MapView {
+    public void displayMap() {
+  String leftIndicator;
+  String rightIndicator;
+  // retreive the game
+  Game game = .getCurrentGame(); 
+  Map map = game.getMap(); // retreive the map from game
+  Location[][] locations = map.getLocations(); // retreive the locations from map
+    // Build the heading of the map
+    System.out.print("  |");
+    for( int column = 0; column < locations[0].length; column++){
+      // print col numbers to side of map
+      System.out.print("  " + column + " |"); 
+    }
+    // Now build the map.  For each row, show the column information
+    System.out.println();
+    for( int row = 0; row < locations.length; row++){
+     System.out.print(row + " "); // print row numbers to side of map
+      for( int column = 0; column < locations[row].length; column++){
+        // set default indicators as blanks
+        leftIndicator = " ";
+        rightIndicator = " ";
+        if(locations[row][column] == map.getCurrentLocation()){
+          // Set star indicators to show this is the current location.
+          leftIndicator = "*"; 
+          rightIndicator = "*"; 
+        } 
+        else if(locations[row][column].isVisited()){
+           // Set < > indicators to show this location has been visited.
+           leftIndicator = ">"; // can be stars or whatever these are indicators showing visited
+           rightIndicator = "<"; // same as above
+        }
+       System.out.print("|"); // start map with a |
+        if(locations[row][column].getScene() == null)
+        {
+             // No scene assigned here so use ?? for the symbol
+             System.out.print(leftIndicator + "??" + rightIndicator);
+        }
+        else
+          System.out.print(leftIndicator
+             + locations[row][column].getScene().getMapSymbol()
+             + rightIndicator);
+      }
+     System.out.println("|");
+    }
+ }
+}
