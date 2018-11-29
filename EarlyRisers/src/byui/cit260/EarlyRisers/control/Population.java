c5aeedfe35660e301c4f5f85fc3275aeaf837cc6
Heather <Heather@DESKTOP-1NKH1N6>
2018-11-29 14:35:09 -0700

    Working on population and menu changes

--- EarlyRisers/src/byui/cit260/EarlyRisers/control/Population.java
+++ EarlyRisers/src/byui/cit260/EarlyRisers/control/Population.java
@@ -6,6 +6,10 @@
 
 //import java.util.Random;
 package byui.cit260.EarlyRisers.control;
+import byui.cit260.EarlyRisers.main.EarlyRisers;
+import byui.cit260.EarlyRisers.model.InventoryItem;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -17,15 +21,24 @@ public class Population {
      * @param population
      * @return
      */
-    public static double calcGrowth(double population) {
-        //Get random number 1-5 for percent of population increase
-
+    public double calcGrowth() {
+      int population = InventoryItem.this.population;
+      
+      public void setSetItems(ArrayList<InventoryItem> setItems) {
+        this.setItems = setItems;
+    }
+      
+      
+      public void addToList( int i ) { list.add(i); }
+      public int getValueAtIndex( int index ) { return list.get( index ); }
+        
+//Get random number 1-5 for percent of population increase
            Random percent = new Random();
            int w = percent.nextInt(5);
            ++w;
            System.out.println(w); 
 //Define invalid variables
-        if (population <= 0) { //if population is under 0
+        if ( population <= 0) { //if population is under 0
             return -1;
         }
         if (population > 300) {//if population grows bigger than 300
@@ -40,26 +53,29 @@ public class Population {
         //Calculate population by taking the beginning population number and taking the 
 //random percent times it by 100 times the population and adding that number to 
 //the total. 
-        double result  = population + ((w/100)*population);
+        double result = population + ((w/100)*population);
         return Math.round(result);
  
 }
-    public static double calcGrowthTithes(double population, double tithes, double harvest){
-     if (tithes >= harvest*.10) {
+    public static double calcGrowthTithes(){
+    ArrayList<InventoryItem> population = null;
+    ArrayList<InventoryItem> tithes = null;
+    ArrayList<InventoryItem> wheat = null;
+        if (tithes >= wheat*.10) {
         Random percent = new Random();
            int random = percent.nextInt(10);
            ++random;
            System.out.println(random);
            double result = population +((random/100)*population);
         return Math.round(result);}
-    if (tithes <= harvest*.06) {
+    if (tithes <= wheat*.06) {
         Random percent = new Random();
            int random = percent.nextInt(6);
            ++random;
            System.out.println(random);
            double result = population +((random/100)*population);
         return Math.round(result);}
-    else if (tithes >= harvest*.04) {
+    else if (tithes >= wheat*.04) {
         Random percent = new Random();
            int random = percent.nextInt(10);
            ++random;
