37931172cb888d15d5bbed73b775acf2eaae53a5
hezz222 <43969113+hezz222@users.noreply.github.com>
2018-11-28 19:34:15 -0700

    Update GameControl.java

--- EarlyRisers/src/byui/cit260/EarlyRisers/control/GameControl.java
+++ EarlyRisers/src/byui/cit260/EarlyRisers/control/GameControl.java
@@ -56,51 +56,62 @@ return 1;
 
     
 
-    public static void createTools() {
-        
+   public static void createTools() {
+        InventoryItem location = new InventoryItem();
         ArrayList<InventoryItem> tools = new ArrayList<InventoryItem>();
-        tools.add(new InventoryItem("Plows", 1));
+        tools.add(new InventoryItem("Plows", 0));
         tools.add(new InventoryItem("", 0));
-        tools.add(new InventoryItem("Shovel", 2));
-        tools.add(new InventoryItem("Scythe", 1));
-        tools.add(new InventoryItem("", 3));
-       
+        tools.add(new InventoryItem("Shovel", 0));
+        tools.add(new InventoryItem("Scythe", 0));
+        tools.add(new InventoryItem("", 0));
+
+        location.setItems(tools);
     }
 
     public static void createWheat() {
+        InventoryItem location = new InventoryItem();
         ArrayList<InventoryItem> wheat = new ArrayList<InventoryItem>();
         wheat.add(new InventoryItem("Wheat Bushels", 3000));
-
+        location.setItems(wheat);
     }
 
     public static void createAnimals() {
+       InventoryItem location = new InventoryItem();
         ArrayList<InventoryItem> animals = new ArrayList<InventoryItem>();
         animals.add(new InventoryItem("Rats", 50));
         animals.add(new InventoryItem("Oxs", 0));
         animals.add(new InventoryItem("Horses", 0));
-
+        location.setItems(animals);
     }
 
     public static void createPopulation() {
+        InventoryItem location = new InventoryItem();
         ArrayList<InventoryItem> population = new ArrayList<InventoryItem>();
         population.add(new InventoryItem("Population", 100));
+        location.setItems(population);
     }
 
     public static void createTithing() {
+        InventoryItem location = new InventoryItem();
         ArrayList<InventoryItem> tithing = new ArrayList<InventoryItem>();
         tithing.add(new InventoryItem("Tithing", 0));
+        location.setItems(tithing);
     }
 
     public static void createLand() {
+        InventoryItem location = new InventoryItem();
         ArrayList<InventoryItem> land = new ArrayList<InventoryItem>();
         land.add(new InventoryItem("Land", 1000));
+        location.setItems(land);
     }
 
     public static void createCropData() {
+        InventoryItem location = new InventoryItem();
         ArrayList<InventoryItem> cropData = new ArrayList<InventoryItem>();
         cropData.add(new InventoryItem("Year", 0));
         cropData.add(new InventoryItem("CropYield", 0));
         cropData.add(new InventoryItem("Dead", 0));
         cropData.add(new InventoryItem("Acres Planted", 0));
+        location.setItems(cropData);
     }
 }
